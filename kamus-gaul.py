980719efd4eb00482ae298b441911616b460f387
yriihrtoo <143616968+yriihrtoo@users.noreply.github.com>
2023-09-20 20:31:30 +0700

    Update kamus-gaul.py
    aku menambahkan looping

--- kamus-gaul.py
+++ kamus-gaul.py
@@ -7,9 +7,10 @@ while True:
          word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")  
             
             if word in meme_dict.keys():
-    # Apa yang harus kita lakukan jika kata itu ditemukan?
-    print(meme_dict[word])
-    break
-else:
-    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
-    print("INVALID WORD")
+                # Apa yang harus kita lakukan jika kata itu ditemukan?
+                print(meme_dict[word])
+                break
+            else:
+                # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+                print("INVALID WORD")
+            
